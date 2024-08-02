879b173adc7c7ecc9729fca1d0c830478ea5f24a
BenH235 <123939851+BenH235@users.noreply.github.com>
2024-08-02 09:44:50 +0100

    Updated cross validation function in model_selection.py

--- forecasting/model_selection.py
+++ forecasting/model_selection.py
@@ -1,29 +1,38 @@
 # Functions for train/test splits & cross validation
 # We can output summary tables for key forecasting metrics (i.e., MAPE/MAE/...)
 
-def cross_val(data, n_splits, test_size, model=None):
-    
+from .models import prophet_forecast
+from sklearn.model_selection import TimeSeriesSplit
+import pandas as pd
+
+
+def cross_val(data, n_splits, test_size, model):
     """
-    Cross validation (k-fold)
+    Cross validation (k-fold) for time series data.
+
     Inputs:
         data (pandas.DataFrame): Univariate time series dataset.
         n_splits (int): Number of folds.
         test_size (int): Forecast horizon during each fold.
-        model: Time series model to evaluate.
+        model: Valid model function
+
     Outputs:
         Cross validation summary (pandas.DataFrame)
     """
-    
-    tscv = TimeSeriesSplit(n_splits=n_splits, test_size=test_size)
-    
-    cross_val = tscv.split(data)
-    
-    cv_summary={}
-    for fold, (train, test) in enumerate(cross_val):
 
+    tscv = TimeSeriesSplit(n_splits=n_splits, test_size=test_size)
+
+    cross_val_idx = tscv.split(data)
+
+    cv_summary = {}
+    for fold, (train, test) in enumerate(cross_val_idx):
         cv_output = data.copy().iloc[test].reset_index(drop=True)
-        # Fake forecast
-        cv_output['forecast'] = 5
+
+        # **Need to ensure model inputs are consistent across different models**
+        forecast = model(data.copy().iloc[train].reset_index(drop=True), len(test))
+
+        cv_output['forecast'] = forecast.yhat.values
+        cv_output['fold'] = fold
         cv_summary[fold] = cv_output
-    
+
     return pd.concat(cv_summary)
